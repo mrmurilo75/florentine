3a5ed97097752c006091f54f788ee30bbae3ddba
Murilo Rosa <mrmurilo75@gmail.com>
2023-03-20 18:38:49 +0000

    improve Account admin

--- florentine/core/admin.py
+++ florentine/core/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from florentine.core.models import Account, Category, Transaction
 
-admin.site.register(Account)
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("name", "current_value")
+    list_filter = ("owner",)
+
+
 admin.site.register(Category)
 admin.site.register(Transaction)
